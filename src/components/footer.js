ebb4bdb62788f07069902ca8968ebf6fc911419c
Vincent Widjaya <vwidjaya@uw.edu>
2019-05-17 02:41:41 -0700

    change footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,15 +3,27 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">
-            &copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. 
-            Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
-        </p>
         <ul className="icons">
-            <li><a href="mailto:vrwidjaya@gmail.com" className="icon fa-envelope"><span className="label">Email</span></a></li>
-            <li><a href="https://www.facebook.com/V9.98R" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="https://www.instagram.com/vincentrw/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="https://github.com/vwidjaya/" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li>
+                <a href="mailto:vrwidjaya@gmail.com" className="icon fa-envelope" target="_blank">
+                    <span className="label">Email</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/V9.98R" className="icon fa-facebook" target="_blank">
+                    <span className="label">Facebook</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/vincentrw/" className="icon fa-instagram" target="_blank">
+                    <span className="label">Instagram</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/vwidjaya/" className="icon fa-github" target="_blank">
+                    <span className="label">GitHub</span>
+                </a>
+            </li>
           </ul>
     </footer>
 )
